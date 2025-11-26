5d4bf1889f322a34d005f39580962386be0d3afb
Oleg Fateev <fateev010405@gmail.com>
2025-11-27 00:10:11 +0300

    hotfix

--- src/utils/sendDataToServer.js
+++ src/utils/sendDataToServer.js
@@ -7,14 +7,16 @@ export const sendDateToServer = (data, images, navigate, setIsLoading) => {
 	console.log('Данные для отправки:', postData);
 
 	setIsLoading(true);
-	// https://octs.guap.ru/services/n8n/webhook/webhook/05b6e16a-be07-4613-855d-fd185fae3a77
-	fetch('', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
+	fetch(
+		'https://octs.guap.ru/services/n8n/webhook/webhook/05b6e16a-be07-4613-855d-fd185fae3a77',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ postData }),
 		},
-		body: JSON.stringify({ postData }),
-	}).then(r => {
+	).then(r => {
 		console.log(r);
 		if (r.status === 200) {
 			navigate('/success');
